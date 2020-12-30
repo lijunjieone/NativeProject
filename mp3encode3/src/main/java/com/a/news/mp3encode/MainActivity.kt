bef22514ba2d5bd498a3dbba86349337404b9821
lijunjie <lijunjie@windimg.com>
2020-12-30 15:55:12 +0800

    增加新的jni工程

---
+++ mp3encode3/src/main/java/com/a/news/mp3encode/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.a.news.mp3encode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_text).text = stringFromJNI()
+
+    }
+
+
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
